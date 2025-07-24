779df53fdf081b5fadc761e4f62a90e566cd19cc
iwancilegon <cilegonlebihbaik@gmail.com>
2025-07-25 04:16:10 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCehTIXl13Wk4BDsuRsolqLxv2HYvlrLU0",
+  authDomain: "afiliatorkreatif.firebaseapp.com",
+  projectId: "afiliatorkreatif",
+  storageBucket: "afiliatorkreatif.appspot.com",
+  messagingSenderId: "397456582300",
+  appId: "1:397456582300:web:5369b2931802bf13dd2f59"
+};
